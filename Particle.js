54569d18c00c78ffa70b0a6ac48140aae3c07b68
Ronald Avendaño <32528023+ravendano014@users.noreply.github.com>
2025-06-01 20:45:39 -0600

    Update Particle.js
    Toggle Trails
    Anti Matter

--- Particle.js
+++ Particle.js
@@ -1,23 +1,16 @@
-//A Javascript implementation of http://www.nowykurier.com/toys/gravity/src.zip - Particle.as
-//This project aims to recreate nowykurier's famous Gravity Toy, found here:
-//http://www.nowykurier.com/toys/gravity/gravity.html
-
-
-function Particle(mass, x, y, vx, vy){
-	this.mass = mass;
-	this.x = x;				//x coordinate
-	this.y = y;				//y coordinate
-	this.px = x;			//previous x coordinate	- used only with paths
-	this.py = y;			//previous y coordinate - used only with paths
-	this.vx = vx;			//x component of velocity
-	this.vy = vy;			//y component of velocity
-	this.ax = 0;			//x component of acceleration
-	this.ay = 0;			//y component of acceleration
-	this.collided = false;
-	this.color = [];
-	this.color[0] = 0xFF;
-	this.color[1] = Math.round(256/(1+Math.pow(this.mass/100000, 1)));	//is pow necessary?
-	this.color[2] = Math.round(256/(1+Math.pow(this.mass/10000, 1)));				//is pow necessary?
-	this.color[3] = (0xFF0000 + (this.color[1]<<8) + this.color[2]).toString(16);
-	this.radius = Math.log(Math.E+mass/1000);
+function Particle(t, s, h, i, o, a) {
+    (this.mass = t),
+        (this.x = s),
+        (this.y = h),
+        (this.vx = i),
+        (this.vy = o),
+        (this.ax = 0),
+        (this.ay = 0),
+        (this.type = a || "matter"),
+        (this.color = []),
+        this.type == "matter"
+            ? ((this.color[0] = 255), (this.color[1] = Math.round(256 / (1 + Math.pow(t / 1e5, 1)))), (this.color[2] = Math.round(256 / (1 + Math.pow(t / 1e4, 1)))))
+            : ((this.color[0] = Math.round(256 / (1 + Math.pow(t / 1e4, 1)))), (this.color[1] = Math.round(256 / (1 + Math.pow(t / 1e5, 1)))), (this.color[2] = 255)),
+        (this.color[3] = ((this.color[0] << 16) | (this.color[1] << 8) | this.color[2]).toString(16)),
+        (this.radius = Math.log(Math.E + t / 1e3));
 }
