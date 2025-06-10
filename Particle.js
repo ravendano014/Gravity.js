ae57e28c6faa97ea6a15abc9b14b0bfe1ad8e490
Ronald Avendaño <32528023+ravendano014@users.noreply.github.com>
2025-06-10 16:10:39 -0600

    Update Particle.js
    Particle.min.js

--- Particle.js
+++ Particle.js
@@ -16,16 +16,20 @@ function Particle(t,s,h,i,o,a,n){
     this.isBlackHole = (this.type === "blackhole");
     if (this.isBlackHole) {
         this.color = [0, 0, 0];
-        this.radius = Math.sqrt(t) / 50;
+        this.radius = Math.sqrt(t) / 20;
         this.eventHorizonRadius = (2 * t) / 1000;
         this.accretionDiskRadius = this.eventHorizonRadius * 3;
+        this.gravityInfluenceRadius = this.eventHorizonRadius * 50; // Radio de influencia grande
         this.name = "Black Hole (" + Math.round(t/1e4) + "M☉)";
     } 
     // Neutron star properties
     else if (this.type === "neutron") {
         this.color = [255, 255, 255];
         this.radius = Math.sqrt(t) / 100;
-        this.magnetosphereRadius = this.radius * 10;
+        this.magnetosphereRadius = this.radius * 15;
+        this.gravityInfluenceRadius = this.radius * 100; // Radio de influencia grande
+        this.eventHorizonRadius = 0; // No tiene horizonte de eventos
+        this.accretionDiskRadius = this.radius * 10; // Disco de acreción más pequeño
         this.name = "Neutron Star";
         this.pulsePhase = 0;
     }
